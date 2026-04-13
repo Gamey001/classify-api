fee9afb43c330655fab96af20f34aecf76dde965
gamalieldashuaDataFi <172114081+gamalieldashuaDataFi@users.noreply.github.com>
2026-04-13 15:05:34 +0100

    Always set Access-Control-Allow-Origin header on every response

--- main.py
+++ main.py
@@ -14,6 +14,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+@app.middleware("http")
+async def add_cors_header(request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
+
 GENDERIZE_URL = "https://api.genderize.io/"
 
 
